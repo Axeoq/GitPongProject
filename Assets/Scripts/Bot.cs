3d9a055fc1b74427ef0e2afbe4c4d1af8f469dfa
Axeoq <anthonysuryana2580@gmail.com>
2024-06-10 23:28:14 +0700

    Fourth Commit
    Add Bot for Single Player

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Bot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform ball;
+    public Transform enemy;
+    public float speed = 1f;
+
+    private Vector2 destination;
+
+    public void MoveToBall()
     {
-        
+        destination = new Vector2(enemy.transform.position.x,ball.position.y);
+        transform.position = Vector2.Lerp(enemy.transform.position,destination,speed * Time.deltaTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveToBall();
     }
+    
 }
